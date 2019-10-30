f49f707d8dc77d34cd489052d40217a4e3f9eaf5
Adam Payne <ajpayne238@gmail.com>
2019-10-30 13:34:49 -0400

    password required

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express'); // importing a CommonJS module
 //Import Helmet
 const helmet = require('helmet');
 
+//Import Morgan
+const morgan = require('morgan');
+
 //Import Hubs Router
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -25,9 +28,34 @@ function logVisit(req, res, next){
   next();
 }
 
+function gateKeeper(req, res, next){
+  //data can come in the body, url parameters, query strings, headers
+  //new way of reading data sent by the client 
+  const password = req.headers.password;
+
+  if(!password){
+    res.status(400).json({ message: "Password Required for Access"})
+  } else if (password === 'mellon'){
+    next();
+  } else {
+    res.status(401).json({ Message: "cannot pass!"})
+  }
+
+  // if(password.toLowerCase() === 'mellon'){
+  //   next();
+  // } else {
+  //   res.status(400).json({ Message: "cannot pass!!" });
+  // }
+}
+
 // Global Middleware
+// Depending on where we put this (cascading) it will set up a gate locked by password
+server.use(gateKeeper);
 //Helmet blocks info being leaked in header.
 server.use(helmet()); // Third Party Middleware
+
+//MOrgan
+server.use(morgan('dev')); // Third Party Middleware
 
 //Fill Requests with JSON
 server.use(express.json()); //Built-in Middleware
@@ -35,6 +63,8 @@ server.use(express.json()); //Built-in Middleware
 server.use(dateLogger); // Custom Middleware
 
 server.use(logVisit); // Custom Middleware
+
+
 
 //Use if requesting /api/hubs
 server.use('/api/hubs', hubsRouter);
